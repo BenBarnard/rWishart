8612cb7093db519ab384cd5ee92498cbfddd5af8
BenBarnard <ben_barnard@baylor.edu>
2017-04-12 11:45:05 -0500

    update

--- R/rNonsingularWishart.R
+++ R/rNonsingularWishart.R
@@ -1,11 +1,26 @@
-#' Title
+#' Random Nonsingular Wishart Matrix
 #'
-#' @return
+#' @inherit rWishart
+#' @export
+#'
+#' @examples rNonsingularWishart(2, 20, diag(1, 5))
+rNonsingularWishart <- function(n, df, Sigma, covariance = FALSE, simplify = "array"){
+  replicate(n,
+            rWishart::NonsingularWishart(df, Sigma, covariance),
+            simplify = simplify)
+}
+
+
+#' Nonsingular Wishart Helper Function
+#'
+#' @inherit rWishart
+#' 
 #' @export
 #' @keywords internal
+#' @importFrom stats rWishart
 #'
-#' @examples
-WishFunc <- function(df, Sigma, covariance){
+#' @examples NonsingularWishart(20, diag(1,5))
+NonsingularWishart <- function(df, Sigma, covariance = FALSE){
   x <- stats::rWishart(1, df, Sigma)[ , , 1]
   atr <- attributes(x)
   attributes(x) <- c(atr, n = df + 1)
